a51eca8e9b50130e73fdabf70430b8f76e17f2a0
OroArmor <eliorona@live.com>
2021-01-28 19:54:35 -0800

    Add a bunch more commands

--- commands/commands.js
+++ commands/commands.js
@@ -21,11 +21,46 @@ module.exports = [{
       channel.send(new Discord.MessageEmbed().setTitle('CurseForge').setColor(0x6441A4).setDescription('OroArmor\'s curseforge page: https://www.curseforge.com/members/oroarmor/projects'));
     }
   }, {
+    name: "modrinth",
+    desc: "Links to OroArmor's modrinth page",
+    permissions: any,
+    runs: (user, channel, tokens) => {
+      channel.send(new Discord.MessageEmbed().setTitle('Modrinth').setColor(0x079515).setDescription('OroArmor\'s modrinth page: https://modrinth.com/user/h74rYEcI'));
+    }
+  }, {
+    name: "github",
+    desc: "Links to OroArmor's Github page",
+    permissions: any,
+    runs: (user, channel, tokens) => {
+      channel.send(new Discord.MessageEmbed().setTitle('Github').setColor(0x555555).setDescription('OroArmor\'s Github page: https://github.com/OroArmor'));
+    }
+  }, {
+    name: "bintray",
+    desc: "Links to OroArmor's Bintray repository",
+    permissions: any,
+    runs: (user, channel, tokens) => {
+      channel.send(new Discord.MessageEmbed().setTitle('Github').setColor(0x079515).setDescription('OroArmor\'s Bintray page: https://bintray.com/oroarmor/oroarmor\nOroArmor\'s Bintray Repo: https://dl.bintray.com/oroarmor/oroarmor/'));
+    }
+  }, {
     name: "apply-for-mod",
     desc: "Provides the link to the moderator application page",
     permissions: any,
     runs: (user, channel, tokens) => {
       channel.send("Apply for moderator at: https://forms.gle/F4LPDH7xD96VknY39");
+    }
+  }, {
+    name: "patreon",
+    desc: "Support me on Patreon",
+    permissions: any,
+    runs: (user, channel, tokens) => {
+      channel.send("https://patreon.com/oroarmor");
+    }
+  }, {
+    name: "architectury",
+    desc: "Links to architectury",
+    permissions: any,
+    runs: (user, channel, tokens) => {
+      channel.send(new Discord.MessageEmbed().setTitle('Architectury').setColor(0xc76003).setDescription('Architectury is required to run Netherite Plus. Download the fabric version: https://www.curseforge.com/minecraft/mc-mods/architectury-fabric or the forge version: https://www.curseforge.com/minecraft/mc-mods/architectury-forge.'));
     }
   }, {
     name: "mod",
@@ -35,13 +70,13 @@ module.exports = [{
       if (tokens.length == 1) {
         let message = "No mod provided! Available mods are:```"
         for (let mod in mods) {
-          message += `\n${mods[mod].id}`
+          message += `\n${mods[mod].id}: ${mods[mod].alias}`
         }
         channel.send(message + "```")
       } else {
         var mod = null;
         for (let modIndex in mods) {
-          if (mods[modIndex].id == tokens[1]) {
+          if (mods[modIndex].id == tokens[1] || mods[modIndex].alias == tokens[1]) {
             mod = mods[modIndex];
             break;
           }
@@ -49,7 +84,7 @@ module.exports = [{
         if (mod == null) {
           channel.send(`Mod: \`${tokens[1]}\`, is not a valid mod.`)
         } else {
-          channel.send(new Discord.MessageEmbed().setTitle(`${mod.name}`).setDescription(`${mod.extdesc}\n\nFind on Github: ${mod.links.github}\nFind on CurseForge: ${mod.links.curseforge}`));
+          channel.send(new Discord.MessageEmbed().setTitle(`${mod.name}`).setDescription(`${mod.extdesc}\n\nFind on Github: ${mod.links.github}\n${(mod.links.curseforge)? "Find on CurseForge: "+ mod.links.curseforge : ""}\n${(mod.links.modrinth)? "Find on Modrinth: "+ mod.links.modrinth : ""}`));
         }
       }
     }
@@ -61,13 +96,13 @@ module.exports = [{
       if (tokens.length == 1) {
         let message = "My mods are:"
         for (let mod in mods) {
-          message += `\n**${mods[mod].name}**: ${mods[mod].desc}`
+          message += `\n**${mods[mod].name} (${mods[mod].alias})**: ${mods[mod].desc}`
         }
         channel.send(new Discord.MessageEmbed().setTitle(`My Mods`).setDescription(message))
       } else {
         var mod = null;
         for (let modIndex in mods) {
-          if (mods[modIndex].id == tokens[1]) {
+          if (mods[modIndex].id == tokens[1] || mods[modIndex].alias == tokens[1]) {
             mod = mods[modIndex];
             break;
           }
@@ -75,7 +110,7 @@ module.exports = [{
         if (mod == null) {
           channel.send(`Mod: \`${tokens[1]}\`, is not a valid mod.`)
         } else {
-          channel.send(new Discord.MessageEmbed().setTitle(`${mod.name}`).setDescription(`${mod.extdesc}\n\nFind on Github: ${mod.links.github}\nFind on CurseForge: ${mod.links.curseforge}`));
+          channel.send(new Discord.MessageEmbed().setTitle(`${mod.name}`).setDescription(`${mod.extdesc}\n\nFind on Github: ${mod.links.github}\n${(mod.links.curseforge)? "Find on CurseForge: "+ mod.links.curseforge : ""}\n${(mod.links.modrinth)? "Find on Modrinth: "+ mod.links.modrinth : ""}`));
         }
       }
     }
@@ -84,7 +119,22 @@ module.exports = [{
     desc: "How to create an issue",
     permissions: any,
     runs: (user, channel, tokens) => {
-      channel.send(new Discord.MessageEmbed().setTitle("Create an issue").setColor(0xff5500).setDescription("I won't support issues in here, so go to github, https://github.com/OroArmor/Netherite-Plus-Mod/issues, and place an issue. As well, if you dont provide a log or screenshots, I will not help you."));
+      if (tokens.length == 1) {
+        channel.send(new Discord.MessageEmbed().setTitle("Create an issue").setColor(0xff5500).setDescription("I won't support issues in here, so go to github, https://github.com/OroArmor/Netherite-Plus-Mod/issues, and place an issue. As well, if you dont provide a log or screenshots, I will not help you."));
+      } else {
+        var mod = null;
+        for (let modIndex in mods) {
+          if (mods[modIndex].id == tokens[1] || mods[modIndex].alias == tokens[1]) {
+            mod = mods[modIndex];
+            break;
+          }
+        }
+        if (mod == null) {
+          channel.send(`Mod: \`${tokens[1]}\`, is not a valid mod.`)
+        } else {
+          channel.send(new Discord.MessageEmbed().setTitle("Create an issue").setColor(0xff5500).setDescription(`I won't support issues in here, so go to github, ${mod.links.github}/issues, and place an issue. As well, if you dont provide a log or screenshots, I will not help you.`));
+        }
+      }
     }
   },
   //dud commands (dont run from here as they are special)
--- configs/mods.js
+++ configs/mods.js
@@ -1,9 +1,11 @@
 module.exports = [{
     name: "Netherite Plus",
     id: "netherite_plus",
+    alias: "np",
     desc: "Adds more netherite features to Minecraft.",
     links: {
       github: "https://github.com/OroArmor/Netherite-Plus-Mod",
+      modrinth: "https://modrinth.com/mod/netherite-plus-mod",
       curseforge: "https://www.curseforge.com/minecraft/mc-mods/netherite-plus-mod"
     },
     extdesc: "Netherite is an amazing feature, but I felt that it was lacking some depth in Vanilla Minecraft, so I created this mod to add netherite features to all current tools. As of now, there are netherite equivalents for the elytra, bow, crossbow, anvil, fishing rod, trident, shulker boxes, horse armor, and shields. Some added features are fake netherite blocks for building, lava fishing with the netherite fishing rod, and xp reduction in the netherite anvil, which never breaks. All of these features are configurable and can be turned on and off."
@@ -11,11 +13,22 @@ module.exports = [{
   {
     name: "Slime Block in Redstone Tab",
     desc: "Reorginizes the creative inventory with configurable groups and items.",
+    alias: "sb",
     id: "slime_block",
     links: {
       github: "https://github.com/OroArmor/SlimeBlockInRedstoneTab",
       curseforge: "https://www.curseforge.com/minecraft/mc-mods/slime-block-in-the-redstone-tab"
     },
     extdesc: "The creative inventory is extremely cluttered and needs updating. This client-side mod allows you to move any item or tag to any tab, and creating new tabs. There are two config files that can be dynamically changed and updated with the /reload command in Minecraft. Check Github for a more in depth explaination of the config files."
+  },
+  {
+    name: "Oro-Config",
+    desc: "Simple JSON based config library with command and mod menu support",
+    alias: "oc",
+    id: "oro_config",
+    links: {
+      github: "https://github.com/OroArmor/Oro-Config"
+    },
+    extdesc: "Simple JSON based config library with command and mod menu support"
   }
 ]
